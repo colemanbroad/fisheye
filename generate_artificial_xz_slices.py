a3bcf0d43cf5e63cfa843474875f7d580999cb16
Coleman Broaddus <broaddus@mpi-cbg.de>
2018-06-19 16:28:01 +0200

    added res063. comparing artificial xz slices w & w/out aniso blur.

--- generate_artificial_xz_slices.py
+++ generate_artificial_xz_slices.py
@@ -2,7 +2,7 @@ from segtools.defaults.ipython_local import *
 from scipy.ndimage import convolve
 import string
 import random
-
+from segtools import cell_view_lib as view
 
 def k_random_inds(n,k):
   "n choose k. returns an ndarray of indices. choose without replacement. k>n equiv to k=n."
@@ -86,6 +86,30 @@ def compare1(img=None, img2=None):
   iss4  = Stack(img2[...,::5]) # downsample x
   return iss1, iss2, iss3, iss4
 
+def compare2(img, img2):
+  "img2 = convolve(img, kernel)"
+  img = img[0]
+  img2 = img2[0]
+  x = np.swapaxes(img,  1, 3)
+  y = np.swapaxes(img2, 1, 3)
+  iss1 = Stack([x,y]) # swap x,z
+
+  x = img[...,::5]
+  y = img2[...,::5]
+  iss2 = Stack([x,y])
+  return iss1, iss2
+
+def compare3(img, img2):
+  t,c = 0,0
+  x_ind = random.randint(0,400)
+  x1 =  img[t,c,: ,x_ind] # true xz
+  z_ind = random.randint(0,71)
+  x2 =  img[t,c,z_ind,:    ,::5].T # artifical. downsampled x
+  x3 = img2[t,c,z_ind,:    ,::5].T # artificial. aniso blur + downsampled x.
+  print(x1.shape, x2.shape, x3.shape)
+  cat = np.concatenate
+  view.imshowme(cat([x1,x2,x3],0))
+
 def downsample_and_plot(img):
   def random_inds_from_axis(img, axis, nsample):
     ss = [slice(None)]*img.ndim
@@ -141,7 +165,6 @@ learn the following functions:
 
   _end = '_end_'
 
-  
 ## Mon Jun 18 14:42:20 2018
 
 Refactoring into functions. We want to apply the psfs coming from `look_at_psf.py`.
@@ -165,11 +188,25 @@ Now we apply the convolution to the entire stack...
 And since this is costly we split original into load_img__apply_psf and downsample_and_plot.
 
 `compare1` shows how applying the convolution makes x way too blurry.
-Simple downsampling of x without blurring gives the most convincing yz look alike.
-1. The convolution is applied to make the effective psf isotropic, but this is done ontop of the image noise.
+Simple downsampling of x without blurring gives the most convincing yz lookalike.
+1. The convolution is applied to make the effective psf isotropic, but this is done on top of the image noise.
 2. This has the effect of hiding noise and the high frequency signal.
 3. Ideally we would first denoise, then apply the pure-anisotropic psf, then *renoise*!
     This assumes noise happens mostly at the end, after the psf has been applied.
-    True, for finite sampling noise and detector noise?
+    Which is True, for finite sampling noise and detector noise?
+
+## Tue Jun 19 11:00:16 2018
+
+We should not just apply random operations to the psf to make iss3 and iss4 look less blurry.
+Potential causes for this problem are:
+1. We should denoise img, apply psf, renoise img for reasons outlined above.
+2. We should denoise *the psf* (at what stage? before separating anisotropic part?)
+    Is this a real explanation? Does the additive noise have the effect of broadening our psfs?
+
+Let's try denoising the psfs first when they are created. See `look_at_psf.py`.
+Denoising + background subtraction leads to nice psfs.
+But the true xz and artificial xz slices still don't look identical.
+And in fact the artificial xz made by downsampling *without* anisotropic blur look better!
+This is because they have the proper noise statistics. See `res063` = compare3().
 
 """
